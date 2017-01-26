a1e1cd54b43de6afb426a1e8ab251e15cff4312f
Simon Lee <leesimon18@gmail.com>
2017-01-26 16:31:07 -0600

    Completed the Web Caesar page for encryption

--- main.py
+++ main.py
@@ -16,17 +16,41 @@
 #
 import webapp2
 import caesar
+import cgi
+
+def build_page(original_textarea, encrypt_textarea, rotation):
+    title = "<h1>Enter your message into Web Caesar:</h1>"
+    input_rotation = "<label><strong>Enter a rotation number: </strong></br><input type='number' name='rotation' value='{}'></label>".format(rotation)
+    original_textarea = "<label><strong>Enter in your message: </strong></br> <textarea name='original_message' rows='8' cols='55'>" + original_textarea + "</textarea></label>"
+    encrypt_textarea = "<label><strong>Encrypted message: </strong></br> <textarea name='encrypt_message' rows='8' cols='55'>" + encrypt_textarea + "</textarea></label>"
+
+    table = "<table><tr><td>" + original_textarea + "</td><td>" + encrypt_textarea + "</td></tr></table>"
+
+    submit = "<input type='submit'/>"
+    form = "<form method='post'>" + input_rotation + "</br></br>" + table + "</br></br>" + submit + "</form>"
+    content = title + form
+    return content
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = "Hello World!"
-        encrypted_message = caesar.encrypt(message, 13)
-
-        textarea = "<textarea>"+encrypted_message+"</textarea>"
-        submit = "<input type='submit'/>"
-        form = "<form>" + textarea + "</br></br>" + submit + "</form>"
-        content = form
+        content = build_page("", "","")
         self.response.write(content)
+
+    def post(self):
+        original_message = ""
+        rotation = 0
+
+        if self.request.get("original_message") != "":
+            original_message = self.request.get("original_message")
+
+        if self.request.get("rotation") != "":
+            rotation = int(self.request.get("rotation"))
+        encrypted_message = caesar.encrypt(original_message, rotation)
+        escaped_message = cgi.escape(encrypted_message)
+        content = build_page(original_message, escaped_message, int(rotation))
+
+        self.response.write(content)
+        # self.response.write("<strong>Secret Message: </strong>" + encrypted_message)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
